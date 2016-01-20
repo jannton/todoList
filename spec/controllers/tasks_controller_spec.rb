15c979db005c3f88a72568746e58ebbc35e80170
janntonn <janntonn@gmail.com>
2016-01-20 00:14:23 -0800

    wrote controler action tests for index, create, new, and complete. also updated view

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -2,9 +2,33 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe TasksController, :type => :controller do
-	render_views
-	
-	it "route POST /tasks to tasks"
 
+
+	describe "GET index" do
+		it "returns home page" do
+			get 'index'
+			expect(response).to be_success
+		end
+	end
+
+	describe 'POST create' do 
+		it "route POST /tasks to tasks#create" do
+			expect(post: '/tasks').to route_to('tasks#create')
+		end
+	end
+
+
+	describe 'GET new' do 
+		it "route GET /tasks to tasks#new" do
+			expect(get: '/tasks/new').to route_to('tasks#new')
+		end
+	end
+
+	describe "PUT complete" do
+		it "updates boolean values" do 
+			expect(put: '/tasks/completed').to route_to('tasks#completed')
+		end
+	end
+	
 
 end
